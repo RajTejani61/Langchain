837988023b2077782872403bf40c4fdf7e98a7b8
Raj Tejani <rjtejani00@gmail.com>
2026-01-01 18:12:22 +0530

    Adding parallel llm execution using Send

--- Projects/Parallel_Execution.py
+++ Projects/Parallel_Execution.py
@@ -1,23 +1,27 @@
 from langchain_groq import ChatGroq
 from langgraph.graph import StateGraph, START, END, MessagesState
+from langgraph.types import Send, Command
 from langchain_core.messages import HumanMessage
 
 from pydantic import BaseModel, Field
 from typing import List
+from typing_extensions import Annotated
+from operator import add
 
 from dotenv import load_dotenv
 load_dotenv()
 
 
 class ResearchState(MessagesState):
-    main_query: str 
-    sub_questions: List[str]    
-    research_notes: List[str]
+    main_query: str
+    sub_questions: List[str]
+    sub_query: str | None
+    research_notes: Annotated[List[str], add]
     final_report: str
 
 
+
 class SubQuestions(BaseModel):
-    """Planning schema"""
     sub_questions: List[str] = Field(
         description="4–6 focused research sub-questions"
     )
@@ -33,74 +37,75 @@ def supervisor_agent(state: ResearchState):
     plan = planner.invoke(
         f"""
         Break the following question into 4–6 research sub-questions.
-        
+
         Question:
-        {state['main_query']}
+        {state["main_query"]}
         """
     )
-    
+
     return {
-        "sub_questions": plan.sub_questions, # type: ignore
+        "sub_questions": plan.sub_questions # type: ignore
     }
 
 
-def sub_query_research_agent(sub_query: str) -> str:
-    """
-    Research a single sub-question.
-    """
+def sub_query_research(state: ResearchState):
+    sub_query = state["sub_query"]
+
     prompt = f"""
     You are a research agent.
     Provide factual findings in 4–5 sentences.
-    
+
     Topic: {sub_query}
     """
-    
+
     response = llm.invoke(prompt)
-    
-    return f"Topic: {sub_query} \n {response.content}"
+
+    return {
+        "research_notes": [
+            f"Topic: {sub_query}\n{response.content}"
+        ]
+    }
 
 
-def run_research(state: ResearchState):
-    notes = []
-    
-    for q in state["sub_questions"]:
-        result = sub_query_research_agent(q)
-        notes.append(result)
-    
-    return {"messages": notes}
+def parallel_query_research(state: ResearchState):
+    return Command(
+        goto=[
+            Send("sub_query_research", {"sub_query": q})
+            for q in state["sub_questions"]
+        ]
+    )
 
 
 def create_final_report(state: ResearchState):
-    
     prompt = f"""
     Write a structured research report.
-    
+
     Main Question:
-    {state['main_query']}
-    
+    {state["main_query"]}
+
     Research Findings:
     {'\n\n'.join(state["research_notes"])}
     """
-    
+
     final = llm.invoke(prompt)
-    
+
     return {
         "final_report": final.content
     }
 
-
 graph = StateGraph(ResearchState)
 
 graph.add_node("planner", supervisor_agent)
-graph.add_node("tools", run_research)
+graph.add_node("parallel_query_research", parallel_query_research)
+graph.add_node("sub_query_research", sub_query_research)
 graph.add_node("final_report", create_final_report)
 
 graph.add_edge(START, "planner")
-graph.add_edge("planner", "tools")
-graph.add_edge("tools", "final_report")
+graph.add_edge("planner", "parallel_query_research")
+graph.add_edge("sub_query_research", "final_report")
 graph.add_edge("final_report", END)
 
-app =  graph.compile()
+app = graph.compile()
 
 query = "What are the benefits and challenges of artificial intelligence in healthcare?"
 
@@ -108,8 +113,9 @@ for response in app.stream({
     "messages": [HumanMessage(content=query)],
     "main_query": query,
     "sub_questions": [],
+    "sub_query": None,
     "research_notes": [],
     "final_report": ""
 }):
-    print("\n===== RESPONSE =====")
+    print("\n\n")
     print(response)
